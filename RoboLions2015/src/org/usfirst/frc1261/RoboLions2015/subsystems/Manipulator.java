8c25e2174022493b80e994f9fbb9ac6c34750347
Terryprhsrobotics <terry.k@prhsrobotics.com>
2015-02-06 15:37:34 -0500

    mostly code formatting

--- RoboLions2015/src/org/usfirst/frc1261/RoboLions2015/subsystems/Manipulator.java
+++ RoboLions2015/src/org/usfirst/frc1261/RoboLions2015/subsystems/Manipulator.java
@@ -10,46 +10,42 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Manipulator extends Subsystem {
-    
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
 	DoubleSolenoid forkPiston = RobotMap.forkSolenoid;
 	AnalogInput transducer = new AnalogInput(1);
-	
+
 	double leastAmountOfPressureNeeded = 100;
-	
+
 	private static final double VOLTS_TO_PRESSURE_FACTOR = 15.0;
-	
-	public double getPressure(){
+
+	public double getPressure() {
 		return transducer.getVoltage() * VOLTS_TO_PRESSURE_FACTOR;
 	}
-	
-	public boolean getQuickkStatus(){
-		if((transducer.getVoltage() * VOLTS_TO_PRESSURE_FACTOR) >= leastAmountOfPressureNeeded ){
+
+	public boolean getQuickkStatus() {
+		if ((transducer.getVoltage() * VOLTS_TO_PRESSURE_FACTOR) >= leastAmountOfPressureNeeded) {
 			return true;
-		}
-		else {
+		} else {
 			return false;
 		}
-		
-		
 	}
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
-    
-    public void pistonOut(){
-    	forkPiston.set(DoubleSolenoid.Value.kForward);
-    }
-    
-    public void pistonIn(){
-    	forkPiston.set(DoubleSolenoid.Value.kReverse);
-    }
-    
-    public void pistonOff(){
-    	forkPiston.set(DoubleSolenoid.Value.kOff);
-    }
-}
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
 
+	public void pistonOut() {
+		forkPiston.set(DoubleSolenoid.Value.kForward);
+	}
+
+	public void pistonIn() {
+		forkPiston.set(DoubleSolenoid.Value.kReverse);
+	}
+
+	public void pistonOff() {
+		forkPiston.set(DoubleSolenoid.Value.kOff);
+	}
+}
