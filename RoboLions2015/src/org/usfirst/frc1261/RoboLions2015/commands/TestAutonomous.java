2a5a3fecd48c86dfb8e8e8535318b43c818d489a
Jason Wu <jasonwupilly1997@gmail.com>
2015-02-11 16:30:49 -0500

    backup square test autonomous tweaked

---
+++ RoboLions2015/src/org/usfirst/frc1261/RoboLions2015/commands/TestAutonomous.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc1261.RoboLions2015.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+import org.usfirst.frc1261.RoboLions2015.commands.*;
+/**
+ *
+ */
+public class TestAutonomous extends CommandGroup {
+    
+    public  TestAutonomous() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    	addSequential(new AutoGyroDriveForward(5));
+    	addSequential(new AutoGyroRightTurn(84));
+    	addSequential(new AutoGyroDriveForward(5));
+    	addSequential(new AutoGyroRightTurn(84));
+    	addSequential(new AutoGyroDriveForward(5.8));
+    	addSequential(new AutoGyroRightTurn(84));
+    	addSequential(new AutoGyroDriveForward(5));
+    	addSequential(new AutoGyroRightTurn(84));
+    	addSequential(new AutoGyroDriveForward(5));
+    	addSequential(new AutoGyroRightTurn(84));
+    	addSequential(new AutoGyroDriveForward(5));
+    	addSequential(new AutoGyroRightTurn(84));
+    	addSequential(new AutoGyroDriveForward(5.8));
+    	addSequential(new AutoGyroRightTurn(84));
+    	addSequential(new AutoGyroDriveForward(5.8));
+    	addSequential(new AutoGyroRightTurn(84));
+    	/*
+    	addSequential(new AutoGyroRightTurn(96));
+    	addSequential(new AutoGyroDriveForward(5));
+    	addSequential(new AutoGyroRightTurn(96));
+    	addSequential(new AutoGyroDriveForward(5.8));
+    	addSequential(new AutoGyroRightTurn(96));
+    	addSequential(new AutoGyroDriveForward(5));
+    	addSequential(new AutoGyroRightTurn(96));
+    	*/
+    	
+    }
+}
